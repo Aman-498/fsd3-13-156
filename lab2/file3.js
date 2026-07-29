cfbb2601f334cc86a722a6efd7cad510cb54f78d
Aman <7129aman@gmail.com>
2026-07-29 12:00:38 +0530

    add three files to stimulate

---
+++ lab2/file3.js
@@ -0,0 +1,24 @@
+import { readFile, writeFile,appendFile} from "fs/promises";
+const writeData =async  (fname, contents) => {
+await writeFile(fname,contents);
+console.log("File written"); 
+};
+
+const readData = async (fname) => {
+const data = await readFile(fname,'utf-8');
+console.log('file contents');
+console.log(data);
+
+};
+const appendData = async (fname, contents)=> {
+    await appendFile(fname, "\n" + contents);
+};
+
+await writeData("happy.txt","I am very happy");
+await readData("happy.txt");
+await appendData("happy.txt", "FSD is Interesting");
+await readData("happy.txt");
+
+
+
+
