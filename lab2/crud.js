91f4c62e7b27e4baf63fb67dca0870f2864ff5ca
Aman <7129aman@gmail.com>
2026-08-18 14:47:38 +0530

    add saveCart and add getCart

--- lab2/crud.js
+++ lab2/crud.js
@@ -1,7 +1,20 @@
 import readline from "readline/promises";
+import { writeFile, readFile } from "fs/promises";
 import { stdin, stdout } from "process";
 
+const FILE = "products.json";
+
+const saveCart = async (cart) => {
+    await writeFile(FILE, JSON.stringify(cart, null, 2));
+};
+
+const getCart = async () => {
+    const data = await readFile(FILE, "utf-8");
+    return JSON.parse(data);
+};
+
 const main = async () => {
+
     const cin = readline.createInterface({
         input: stdin,
         output: stdout
@@ -10,23 +23,25 @@ const main = async () => {
     let choice;
 
     do {
-        console.log("\nWelcome to shopping cart 🛒");
-        console.log("1 -------- Add to cart");
-        console.log("2 -------- Show cart");
-        console.log("3 -------- Remove items");
-        console.log("4 -------- Update quantity");
-        console.log("5 -------- Checkout");
-        
+        console.log("\nWelcome to shopping cart 🛍️");
+        console.log("1 --------- Add to cart");
+        console.log("2 --------- Show cart");
+        console.log("3 --------- Remove from cart");
+        console.log("4 --------- Update Quantity");
+        console.log("5 --------- Checkout");
 
-        choice = await cin.question("Enter your choice: ");
+        choice = Number(await cin.question("Enter your choice: "));
 
-        switch(Number (choice)) {
+        console.log("Entered choice:", choice);
+
+        switch (choice) {
+
             case 1:
                 console.log("Add to cart");
                 break;
 
             case 2:
-                console.log("Show cart");
+                console.log("Show cart items");
                 break;
 
             case 3:
@@ -38,15 +53,14 @@ const main = async () => {
                 break;
 
             case 5:
-                console.log("Checkout");
-                process.exit();
+                console.log("See you later");
                 break;
 
             default:
-                console.log("Invalid choice! try again 😶‍🌫️");
+                console.log("Invalid choice, try again");
         }
 
-    } while (choice != "5");
+    } while (choice !== 5);
 
     cin.close();
 };
